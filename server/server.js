f5ebbdab9e45da601559cf72c7df80fc6025ea1f
econgamer <econgamer@gmail.com>
2017-10-06 20:27:24 +0800

    Server set up

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+
+const mongoose = require('mongoose');
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+
+
+
+var connectionUrl = 'mongodb://admin:admin@ds155091.mlab.com:55091/databasetesting';
+
+mongoose.connect(connectionUrl);
+mongoose.connection.once('open', () => console.log('Database is ready!'))
+                    .on('error', (error) => {
+                      console.warn('Warning, error');
+                    });
+
+app.use(express.static(publicPath));
+
+app.get('/', function(req, res){
+  console.log('hihi');
+
+
+});
+
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
